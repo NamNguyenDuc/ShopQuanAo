bf38529396db8b41a3bc1e367e1e3b587ac0cf16
NamND <nguyenducnam1198@gmail.com>
2021-02-25 02:35:24 +0700

    hoan thanh xoa san pham

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -1,11 +1,13 @@
 import ProductApi from '../api/ProductApi';
 import CategoryAPI from '../api/categoryAPI';
+import { $, reRender } from '../utils';
 
 const ProductsPage = {
     async render() {
         const { data: products } = await ProductApi.getAll();
         const { data: categories } = await CategoryAPI.getAll();
         return `
+        <div id="list-products">
             <div class="row">
                 <div class="col-8">
                     <h2>Danh sách sản phẩm</h2>
@@ -37,15 +39,31 @@ const ProductsPage = {
                                 }).join(' ')}</td>
                                 <td>${product.quantity}</td>
                                 <td>
-                                    <a href="/#/editproduct/${product.id}">Edit</a>
-                                    <a href="/#/products/${product.id}">Delete</a>
+                                    <a href="/#/editproduct/${product.id}" class="btn btn-primary">Update</a>
+                                    <button class="btn btn-danger btn-remove remove-product" data-id="${product.id}">Remove</button>
                                 </td>
                             </tr>
                         `
                     }).join(' ')}
                 </tbody>
             </table>
+        </div>
         `
+    }, 
+
+    async afterRender() {
+        const btns = $('#list-products .remove-product');
+        btns.forEach(btn => {
+            const id = btn.dataset.id;
+            console.log(id);
+            btn.addEventListener('click', function() {
+                const question = confirm('Bạn có chắc chắn muốn xóa không?')
+                if(question) {
+                    ProductApi.remove(id);
+                    location.reload();
+                }
+            })
+        })
     }
 }
 
