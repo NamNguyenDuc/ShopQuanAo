9176ba85f42d653772cdfff813159fcf445f4445
NamND <nguyenducnam1198@gmail.com>
2021-02-25 08:46:10 +0700

    Fommart code

--- frontend/src/pages/CategoryPage.js
+++ frontend/src/pages/CategoryPage.js
@@ -6,19 +6,18 @@ const CategoryPage = {
         const { id } = parseRequestUrl();
         const { data: products } = await ProductApi.getAll();
         const result = products.filter(product => product.categoryId == id).map(product => {
-
             return ` 
                  <div class="all-sp">
-                                    <div class="imgg">
-                                        <a href="/#/products/${product.id}"><img src="${product.image}" class="img-fluid" width="100%" height="350px" alt=""></a>
-                                    </div>
-                                    <div class="ten">
-                                        <a href="">${product.name}</a>
-                                        <p>${product.price} <b>$</b> </p>
-                                    </div>
-                                    <div class="text">
-                                        <a href="/#/products/${product.id}"> <button class="nnn">Xem Chi Tiết  <i class="fa fa-eye" aria-hidden="true"></i></button> </a>
-                                    </div>
+                    <div class="imgg">
+                        <a href="/#/products/${product.id}"><img src="${product.image}" class="img-fluid" width="100%" height="350px" alt=""></a>
+                    </div>
+                    <div class="ten">
+                        <a href="">${product.name}</a>
+                        <p>${product.price} <b>$</b> </p>
+                    </div>
+                    <div class="text">
+                        <a href="/#/products/${product.id}"> <button class="nnn">Xem Chi Tiết  <i class="fa fa-eye" aria-hidden="true"></i></button> </a>
+                    </div>
                 </div>
             
             `
