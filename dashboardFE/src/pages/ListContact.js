32080e03f298081f2de67112cd3f4d28f942e978
VietAnh <vietanhbaudz98@gmail.com>
2021-03-04 01:29:05 +0700

    validate lienhe xoa lien he

--- dashboardFE/src/pages/ListContact.js
+++ dashboardFE/src/pages/ListContact.js
@@ -1,11 +1,12 @@
 import ContactApi from "../api/ContactAPI";
+import { $ } from '../utils';
 
 const ListContact = {
     async render() {
-        const { data : listContact } = await ContactApi.getAll();
+        const { data: listContact } = await ContactApi.getAll();
 
         return `
-            <div id="list-products">
+            <div id="list-contacts">
                 <table class="table table-striped table-hover">
                     <thead>
                         <tr>
@@ -14,20 +15,24 @@ const ListContact = {
                             <th scope="col">Email</th>
                             <th scope="col">Số điện thoại</th>
                             <th scope="col">Ghi chú</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                    ${listContact && listContact.map(contact => {
-                        return `
+                    ${listContact.map(contact => {
+            return `
                             <tr>
                                 <td>${contact.id}</td>
                                 <td>${contact.name}</td>
                                 <td>${contact.email}</td>
                                 <td>${contact.phone}</td>
                                 <td>${contact.note}</td>
+                                <td>
+                                    <button class="btn btn-danger btn-remove remove-contact" data-id="${contact.id}">Remove</button>
+                                </td>
                             </tr>
                         `
-                    }).join(' ')}
+        }).join(' ')}
                         
                     </tbody>
                 </table>
@@ -36,7 +41,17 @@ const ListContact = {
     },
 
     async afterRender() {
-        
+        const btns = $('#list-contacts .remove-contact');
+        btns.forEach(btn => {
+            const id = btn.dataset.id;
+            btn.addEventListener('click', function () {
+                const question = confirm('Bạn có chắc chắn muốn xóa không?')
+                if (question) {
+                    ContactApi.remove(id);
+                    location.reload();
+                }
+            })
+        })
     }
 }
 export default ListContact;
